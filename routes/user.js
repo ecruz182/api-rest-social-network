63b6639e4f37aec30c81191c3a251dad58f59aa9
ecruz182 <edcruz182@gmail.com>
2024-06-14 21:24:23 -0500

    create user in database

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 //Importaciones
 import { Router} from "express";
-import { testUser } from "../controllers/user.js";
+import { testUser, register } from "../controllers/user.js";
 const router = Router();
 
 //Definir la rutas
 router.get('/test-user', testUser);
+router.post('/register', register);
 
 //Exportar el Router
 export default router;
